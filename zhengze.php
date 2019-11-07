03a6912527d6df25780be4f9711e5a62fe424cd5
bjhxcc <bjhxcc@yeah.net>
2019-11-07 14:05:09 +0800

    dds

---
+++ zhengze.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Author: dreamsun
+ * @Date: 2019年11月7日
+ * @Time: 下午1:52:12
+ */
+// 检查http或https正则
+$url = "http://www.baidu.com";
+$preg = "/^http(s)?:\\/\\/.+/";
+if (preg_match($preg, $url)) {
+    echo true;
+} else {
+    echo false;
+}
